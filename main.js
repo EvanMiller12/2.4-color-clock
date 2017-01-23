8cef5fc2859f8942185106004f5a7ad9591b8e35
evan miller <emille4@clemson.edu>
2017-01-23 16:18:46 -0500

    fixed color code on hover

--- main.js
+++ main.js
@@ -1,37 +1,75 @@
 (function(){
   "use strict";
 
+var runClock = document.getElementById('hexConversion');
+var isHovering = false;
+
 function clockDisplay(){
 // gives currentTime the value of the current circulating time
 var currentTime = new Date();
+
 //gives hours the current hours only
-var hours =  currentTime.getHours();
-//gives minutes the current minutes only
-var minutes = currentTime.getMinutes();
-//gives seconds the current seconds only
-var seconds = currentTime.getSeconds();
+var hours =  (currentTime.getHours());
+var minutes = (currentTime.getMinutes());
+var seconds = (currentTime.getSeconds());
 //gives the percentage of a minute that the current seconds represents
 var progress = seconds/60 * 100;
-//gives a hexidecimal color that is based on the current second every second
-var colorChange = ("#" + hours + minutes + seconds);
 
-var hexConversion = document.getElementById('hexConversion');
+var hexHours = hours.toString(16);
+var hexMinutes = minutes.toString(16);
+var hexSeconds = seconds.toString(16);
+// console.log(hexHours, hexMinutes, hexSeconds);
+
+var hoursNode = document.getElementById('hours');
+var minutesNode = document.getElementById('minutes');
+var secondsNode = document.getElementById('seconds');
+
+var colorChange = ('#' + hours + minutes + seconds);
+var colorCode = ('#' + hexHours + hexMinutes + hexSeconds);
+
+// var color = '#' + hours + minutes + seconds;
 //selects elements by id and prints out the hours minutes and seconds onto the screen
-document.getElementById('hours').innerHTML = ("0" + hours).slice(-2);
-document.getElementById('minutes').innerHTML = ("0" + minutes).slice(-2);
-document.getElementById('seconds').innerHTML = ("0" + seconds).slice(-2);
-//makes the progress bar get bigger every second according to the percent of progress var
+// document.getElementById('hours').innerHTML = ("00" + hours);
+// document.getElementById('minutes').innerHTML = ("00" + minutes);
+// document.getElementById('seconds').innerHTML = ("00" + seconds);
+
 document.getElementById('progressBar').style.width = progress + "%";
-//selects the background and changes the color according to colorChange var
+
 document.getElementById('background').style.backgroundColor = colorChange;
 
+if(isHovering){
+  hoursNode.innerHTML = hexHours;
+  minutesNode.innerHTML = hexMinutes;
+  secondsNode.innerHTML = hexSeconds;
+}else{
+  hoursNode.innerHTML = ('0' + hours).slice(-2);
+  minutesNode.innerHTML = ('0' + minutes).slice(-2);
+  secondsNode.innerHTML = ('0' + seconds).slice(-2);
 }
+// console.log(hoursNode);
+// hoursNode.innerHTML = hours;
+// minutesNode.innerHTML = minutes;
+// secondsNode.innerHTML = seconds;
+
+
+}
+
+function mouseOverHandler(){
+  isHovering = true;
+}
+
+function mouseOutHandler(){
+  isHovering = false;
+}
+
+runClock.addEventListener('mouseover', mouseOverHandler);
+runClock.addEventListener('mouseout', mouseOutHandler);
+
+
 // makes clock tick every second
 setInterval(clockDisplay, 1000);
 //runs the clockDisplay function
 clockDisplay();
-
-
 
 
 
