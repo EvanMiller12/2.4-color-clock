2f562053485b33215c6004803d53505adc7c5513
evan miller <emille4@clemson.edu>
2017-10-13 11:07:15 -0400

    refactored color clock

--- main.js
+++ main.js
@@ -1,87 +1,51 @@
 (function(){
   "use strict";
 
-var runClock = document.getElementById('hexConversion');
-var isHovering = false;
+  function clockDisplay(){
+ 
+    var currentTime = new Date();
 
-function clockDisplay(){
-// gives currentTime the value of the current circulating time
-var currentTime = new Date();
+    var hours =  ('0' + currentTime.getHours()).slice(-2);
+    var minutes = ('0' + currentTime.getMinutes()).slice(-2);
+    var seconds = ('0' + currentTime.getSeconds()).slice(-2);
 
-//gives hours the current hours only
-var hours =  (currentTime.getHours());
-var minutes = (currentTime.getMinutes());
-var seconds = (currentTime.getSeconds());
-//gives the percentage of a minute that the current seconds represents
-var progress = seconds/60 * 100;
+    var hoursNode = document.getElementById('hours');
+    var minutesNode = document.getElementById('minutes');
+    var secondsNode = document.getElementById('seconds');
 
-var hexHours = hours.toString(16);
-var hexMinutes = minutes.toString(16);
-var hexSeconds = seconds.toString(16);
-// console.log(hexHours, hexMinutes, hexSeconds);
+    var clock = document.getElementById('clock');
+    var colorCode = ('#' + hours + minutes + seconds);
+    
+    clock.style.backgroundColor = colorCode;
+    
+    var progress = seconds/60 * 100;
+    document.getElementById('progressBar').style.width = progress + "%";
 
-var hoursNode = document.getElementById('hours');
-var minutesNode = document.getElementById('minutes');
-var secondsNode = document.getElementById('seconds');
+    if(isHovering){
+      hoursNode.innerHTML = "#" + hours;
+      minutesNode.innerHTML = '0' + minutes;
+      secondsNode.innerHTML = '0' + seconds;
+    }else{
+      hoursNode.innerHTML = hours + ":";
+      minutesNode.innerHTML = minutes + ":";
+      secondsNode.innerHTML = '0' + seconds;
+    }
 
-var colorChange = ('#' + hours + minutes + seconds);
-var colorCode = ('#' + hexHours + hexMinutes + hexSeconds);
+    clock.addEventListener('mouseover', mouseOverHandler);
+    clock.addEventListener('mouseout', mouseOutHandler);
+   }
 
-// var color = '#' + hours + minutes + seconds;
-//selects elements by id and prints out the hours minutes and seconds onto the screen
-// document.getElementById('hours').innerHTML = ("00" + hours);
-// document.getElementById('minutes').innerHTML = ("00" + minutes);
-// document.getElementById('seconds').innerHTML = ("00" + seconds);
+    var isHovering = false;
 
-document.getElementById('progressBar').style.width = progress + "%";
+    function mouseOverHandler(){
+      isHovering = true;
+    }
 
-document.getElementById('background').style.backgroundColor = colorChange;
+    function mouseOutHandler(){
+      isHovering = false;
+    }
 
-if(isHovering){
-  hoursNode.innerHTML = hexHours;
-  minutesNode.innerHTML = hexMinutes;
-  secondsNode.innerHTML = hexSeconds;
-}else{
-  hoursNode.innerHTML = ('0' + hours).slice(-2);
-  minutesNode.innerHTML = ('0' + minutes).slice(-2);
-  secondsNode.innerHTML = ('0' + seconds).slice(-2);
-}
-// console.log(hoursNode);
-// hoursNode.innerHTML = hours;
-// minutesNode.innerHTML = minutes;
-// secondsNode.innerHTML = seconds;
-
-
-}
-
-function mouseOverHandler(){
-  isHovering = true;
-}
-
-function mouseOutHandler(){
-  isHovering = false;
-}
-
-runClock.addEventListener('mouseover', mouseOverHandler);
-runClock.addEventListener('mouseout', mouseOutHandler);
-
-
-// makes clock tick every second
-setInterval(clockDisplay, 1000);
-//runs the clockDisplay function
-clockDisplay();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  setInterval(clockDisplay, 1000);
+ 
+  clockDisplay();
 }());
